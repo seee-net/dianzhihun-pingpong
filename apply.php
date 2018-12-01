128fb5b834b6168a3ce0feaac400c016accd9d16
江艺 <jiangyi2012@outlook.com>
2018-12-01 17:00:30 +0800

    Update apply.php

--- apply.php
+++ apply.php
@@ -5,7 +5,6 @@ $serverName = "localhost";
 $userName = "root";
 $password = "";
 $dbName = "dianzhihun_pingpang";
-echo"！";
     // 创建连接
     $conn = new mysqli($serverName, $userName, $password, $dbName);
     $conn->set_charset('utf8');
@@ -14,7 +13,12 @@ echo"！";
     if ($conn->connect_error) die("连接失败: " . $conn->connect_error);
 
     $sql="INSERT INTO userData(userclass, username, usersex, userphone, userqq) VALUES ('{$_POST['userclass']}','{$_POST['username']}','{$_POST['usersex']}','{$_POST['userphone']}','{$_POST['userqq']}')";
-    $conn->query($sql);
+
+	if ($conn->query($sql) === TRUE) {
+		echo"<script>alert('报名成功！')</script>";
+	} else {
+		echo"<script>alert('报名失败！')</script>";
+	}
 
     //断开连接
     $conn->close();
